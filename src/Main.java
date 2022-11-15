fb50047fce6137fa958c4d31a8970341f6b5a0b5
Courtney Williams <williams.a.courtney@gmail.com>
2022-11-15 16:01:25 -0500

    Soda Machine Add Drink class, and the child soda and milk class update main class

--- src/Main.java
+++ src/Main.java
@@ -20,13 +20,16 @@ public class Main {
         String priceText = input.nextLine();
 
         double price = Double.parseDouble(priceText);
+
         cupcake.setPrice(price);
 
         System.out.println("We are deciding on the price for our redvelvet cupcake. Here is the description:");
         redVelvet.type();
 
         System.out.println("How much would you like to charge for the redvelvet cupcake? (Input a numerical number taken to 2 decimal places)");
-        price = input.nextDouble();
+        priceText = input.nextLine();
+
+        price = Double.parseDouble(priceText);
 
         redVelvet.setPrice(price);
 
@@ -34,7 +37,9 @@ public class Main {
         chocolate.type();
 
         System.out.println("How much would you like to charge for the chocolate cupcake? (Input a numerical number taken to 2 decimal places)");
-        price = input.nextDouble();
+        priceText = input.nextLine();
+
+        price = Double.parseDouble(priceText);
 
         chocolate.setPrice(price);
 
@@ -43,6 +48,59 @@ public class Main {
         cupcakeMenu.add(redVelvet);
 
         cupcakeMenu.add(chocolate);
+
+        /**************************************************************************************************************/
+
+        ArrayList<Drink> drinkMenu = new ArrayList<Drink>();
+
+        Drink water = new Drink();
+        Soda soda = new Soda();
+        Milk milk = new Milk();
+
+        System.out.println("We are in the middle of creating the drink menu. We currently have three types of drinks on" +
+                            "\nthe menu but we need to decide on pricing");
+
+        System.out.println("We are deciding on the price for our bottled water. Here is the description: ");
+        water.type();
+
+        System.out.println("How much would you like to charge for the water bottle?" +
+                "\n(Input a numerical number taken to 2 decimal places)");
+
+        priceText = input.nextLine();
+
+        price = Double.parseDouble(priceText);
+
+        water.setPrice(price);
+
+        System.out.println("We are deciding on the price for our bottle of soda. Here is the description: ");
+        soda.type();
+
+        System.out.println("How much would you like to charge for the bottled soda?" +
+                "\n(Input a numerical number taken to 2 decimal places)");
+
+        priceText = input.nextLine();
+
+        price = Double.parseDouble(priceText);
+
+        soda.setPrice(price);
+
+        System.out.println("We are deciding on the price for our bottle of milk. Here is the description: ");
+        milk.type();
+
+        System.out.println("How much would you like to charge for the bottled milk?" +
+                "\n(Input a numerical number taken to 2 decimal places)");
+
+        priceText = input.nextLine();
+
+        price = Double.parseDouble(priceText);
+
+        milk.setPrice(price);
+
+        drinkMenu.add(water);
+
+        drinkMenu.add(soda);
+
+        drinkMenu.add(milk);
     }
 }
 class Cupcake{
@@ -74,5 +132,38 @@ class Chocolate extends Cupcake{
     @Override
     public void type() {
         System.out.println("A chocolate based cupcake, with chocolate frosting.");
+    }
+}
+
+class Drink{
+    public double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void type(){
+        System.out.println("A bottle of water");
+    }
+
+}
+
+class Soda extends Drink{
+
+    @Override
+    public void type(){
+        System.out.println("A bottle of soda");
+    }
+}
+
+class Milk extends Drink{
+
+    @Override
+    public void type(){
+        System.out.println("A bottle of milk");
     }
 }
